eeb3b6f939e60a1de9f7bfc7cefeebcd6f8f4308
Mateus Cavalcanti <mscavalcan7i@gmail.com>
2021-05-23 17:25:44 -0300

    Port Scan e README atualizado

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Antes de utilizar esse programa, certifique-se de ter a biblioteca socket instal
 
 > pip3 install socket
 
-Para executar o script você deve fornecer um host, uma porta inicio ou limite (opcional -> Nossa parte limite caso você não informe sempre será 3306) e um timer que pode ir de t1 a t5 (opcional -> nosso timer padrão sempre será de 1) 
+Para executar o script você deve fornecer um host, uma porta inicio ou limite (opcional -> Nossa parte limite caso você não informe sempre será 3306 e a inicial será 1) e um timer que pode ir de t1 a t5 (opcional -> nosso timer padrão sempre será de 1) 
 
 Exemplo:
 
+> python3 port_scan.py 127.0.0.1
 > python3 port_scan.py 127.0.0.1 1 65535 t1 
 
  
--- port_scan.py
+++ port_scan.py
@@ -1,39 +1,48 @@
 import socket
 import sys
 
-host = sys.argv[1]
-ports = 0
-timer = 1
+def scan(host, timer):
+    if len(sys.argv) > 4:
+        ports = range(int(sys.argv[2]), int(sys.argv[3]))
+        t = sys.argv[4]
 
-if len(sys.argv) > 4:
-    ports = range(int(sys.argv[2]), int(sys.argv[3]))
-    t = sys.argv[4]
+        if t == "t1":
+            timer = 0.1
+        elif t == "t2":
+            timer = 0.4
+        elif t == "t3":
+            timer = 0.7
+        elif t == "t4":
+            timer = 1
+        else:
+            timer = 2
 
-    if t == "t1":
-        timer = 0.1
-    elif t == "t2":
-        timer = 0.4
-    elif t == "t3":
-        timer = 0.7
-    elif t == "t4":
-        timer = 1
+    elif len(sys.argv) > 3:
+        ports = range(int(sys.argv[2]), int(sys.argv[3]))
+
     else:
-        timer = 2
-
-elif len(sys.argv) > 3:
-    ports = range(int(sys.argv[2]), int(sys.argv[3]))
-
-else:
-    ports = range(int(sys.argv[2]), 3306)
+        ports = range(1, 3307)
 
 
-print(" PORTA | STATUS")
+    print(" PORTA | STATUS")
 
-for porta in ports:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(timer)
+    for porta in ports:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(timer)
 
-    code = client.connect_ex((host, porta))
+        code = client.connect_ex((host, porta))
 
-    if code == 0:
-        print(f' {porta}/tcp | OPEN')
+        if code == 0:
+            print(f' {porta}/tcp | OPEN')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        host = sys.argv[1]
+        ports = 0
+        timer = 1
+        scan(host, timer)
+    else:
+        print("Usage: python3 host")
+        print("or")
+        print("Usage: python3 host starting_port end_port timer")
